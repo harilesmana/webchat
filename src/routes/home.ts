ff59f7761a64a4f18810e33c4f6ea6e57150a2ee
harilesmana <harilesmana878@gmail.com>
2025-03-16 03:04:10 +0700

    Delete src/routes/home.ts

--- src/routes/home.ts
+++
@@ -1,8 +0,0 @@
-import { Elysia } from "@elysiajs/elysia";
-
-export const homeRoutes = new Elysia()
-  .get("/", async ({ cookie, render }) => {
-    const sessionId = cookie.get("auth_session");
-    if (sessionId) return new Response(null, { status: 302, headers: { Location: "/dashboard" } });
-    return render("index.ejs");
-  });
