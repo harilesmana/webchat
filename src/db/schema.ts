7a5a7fc688e101355cfc013abb4b59b5d17d9cc5
harilesmana <harilesmana878@gmail.com>
2025-03-18 23:08:48 +0700

    Delete src/db directory

--- src/db/schema.ts
+++
@@ -1,8 +0,0 @@
-// src/db/schema.ts
-import { mysqlTable, varchar, int } from "drizzle-orm/mysql-core";
-
-export const users = mysqlTable("users", {
-  id: int("id").primaryKey().autoincrement(),
-  username: varchar("username", { length: 255 }).notNull().unique(),
-  password: varchar("password", { length: 255 }).notNull(),
-});
